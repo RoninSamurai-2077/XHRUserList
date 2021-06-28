7b95556e0b651208e2bd637ca770fd9692f6afea
Greg <Gdixon5125@gmail.com>
2021-06-28 16:55:35 -0400

    idk what im doing

--- js/users.js
+++ js/users.js
@@ -1,12 +1,17 @@
 // On page load, pull the users endpoint from https://jsonplaceholder.typicode.com/users using XHR
-(function() {Win.UI.Pages.define("/XHRUserList/index.html",{
-    ready: function(element, option){
+// (function() {Win.UI.Pages.define("/XHRUserList/index.html",{
+//     ready: function(element, option){
+    const xhr = XMLHttpRequest;
+        console.log(this);
+    
         var userList = document.getElementById("userList");
-            Winjs.xhr({url:"https://jsonplaceholder.typicode.com/users", headers: {Accept: "jsonplaceholder"}}).then(function(xhr) {
-                var userList =  jsonplaceholder.parse(xhr.response).d;
-                .map(function (r){return r.id})
-            }
-        );
+        // Winjs.xhr({url:'https://jsonplaceholder.typicode.com/users', headers: {Accept: "jsonplaceholder"}}).then(function(xhr) {
+        //         var userList =  jsonplaceholder.parse(xhr.response).d;
+        // //         .map(function (r){return r.id})
+        //     }
+        // );
+        xhr.open('get', 'https://jsonplaceholder.typicode.com/users');
+        xhr.sent();
 
 // Generate the table of users for #usersTable (include a button/link in the fourth column to let you view an individual record)
 
