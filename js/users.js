fd59e1c74fed6a340c9b494ca9d594710efb5c72
Greg <Gdixon5125@gmail.com>
2021-06-28 16:33:55 -0400

    Using XHR to load data from a remote API

--- js/users.js
+++ js/users.js
@@ -1,5 +1,14 @@
-
 // On page load, pull the users endpoint from https://jsonplaceholder.typicode.com/users using XHR
+(function() {Win.UI.Pages.define("/XHRUserList/index.html",{
+    ready: function(element, option){
+        var userList = document.getElementById("userList");
+            Winjs.xhr({url:"https://jsonplaceholder.typicode.com/users", headers: {Accept: "jsonplaceholdern"}}).then(function(xhr) {
+                var userList =  jsonplaceholder.parse(xhr.response).d;
+            }
+        );
+    }
+
 // Generate the table of users for #usersTable (include a button/link in the fourth column to let you view an individual record)
-// If a button/link in the table is clicked, populate #userView with the full details of the user.
+
+// If a button/link in the table is clickyed, populate #userView with the full details of the user.
 // (bonus points if you make it a nice form as if the user was editable)
