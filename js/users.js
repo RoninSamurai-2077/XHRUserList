4123e75ace965b11a8d83bd68a57fc4aca3ad8b8
Greg <Gdixon5125@gmail.com>
2021-06-29 13:37:30 -0400

    ALMOST THERE

--- js/users.js
+++ js/users.js
@@ -15,14 +15,19 @@ function getEl(id) {
         
         let populate=`<tr><td>${userId}</td> <td>${userName}</td> <td>${userEmail}</td> <td><button type='button'>View</button></td></tr>`;
 
-            getEl('userList').innerHTML + populate;
-
+    getEl('userList').innerHTML + populate;
+    }
+}
         xhr.open('GET', 'https://jsonplaceholder.typicode.com/users' ); 
         xhr.send();
 
 getEl('userList').addEventListener('click', function(event){
-    console.log(event.target.parentNode.parentNode.children [0].innerText); }
-if(event.target.nodeName == 'BUTTON')
+    console.log(event.target.parentNode.parentNode.children [0].innerText); 
+if(event.target.nodeName == 'BUTTON') {
+
+    }
+});
+
 
 
 
