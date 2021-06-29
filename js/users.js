25c2c7864fe9257a961e11ac2f87bcfa0f4ef657
Greg <Gdixon5125@gmail.com>
2021-06-29 13:20:42 -0400

    umm

--- js/users.js
+++ js/users.js
@@ -5,13 +5,13 @@ function getEl(id) {
     let userInfo = [];
     const xhr = new XMLHttpRequest();
     xhr.onload = function(){
-        userInfo = JSON.parse(this.responseText);
+        userInfo = JSON.parse(xhr.response);
             console.log (userInfo);
 
     for(let i = 0; i< userInfo.length; i++){
         let userId = userInfo[i].id;
-        let userName userInfo[1].name;
-        let userEmail userInfo[1].email;
+        let userName = userInfo[1].name;
+        let userEmail = userInfo[1].email;
         
         let populate=`<tr><td>${userId}</td> <td>${userName}</td> <td>${userEmail}</td> <td><button type='button'>View</button></td></tr>`;
 
@@ -21,12 +21,12 @@ function getEl(id) {
         xhr.send();
 
 getEl('userList').addEventListener('click', function(event){
+    console.log(event.target.parentNode.parentNode.children [0].innerText); }
+if(event.target.nodeName == 'BUTTON')
 
-console.log(event.target.parentNode.parentNode.children [0].innerText); if(event.target.nodeName == 'BUTTON')
-    }
-}
+
 
 // Generate the table of users for #usersTable (include a button/link in the fourth column to let you view an individual record)
 
-// If a button/link in the table is clickyed, populate #userView with the full details of the user.
+// If a button/link in the table is clickyed, populate #userView with the full details of theS user.
 // (bonus points if you make it a nice form as if the user was editable)
